218fa1e1eddcbfbf2b8f0f3f12bd423691bf6bc9
DEWdrop11 <93488265+DEWdrop11@users.noreply.github.com>
2022-10-10 16:10:17 +0530

    Update segment.py
    Changes to the frames of this segment.py code

--- Morphology/segment.py
+++ Morphology/segment.py
@@ -1,15 +1,15 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('inp.jpg',0)
+img = cv2.imread('inp.jpg',2)
 
-ret,bin = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+ret,bin = cv2.threshold(img,130,255,cv2.THRESH_BINARY_INV)
 
 kernel = np.ones((3,3),np.uint8)
 opened = cv2.morphologyEx(bin, cv2.MORPH_OPEN, kernel)
 cv2.imshow("opened", opened)
 
-kernel = np.ones((5,5),np.uint8)
+kernel = np.ones((6,6),np.uint8)
 closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel)
 ret, output = cv2.threshold(closed,127,255,cv2.THRESH_BINARY_INV)
 cv2.imshow("segment", output)
